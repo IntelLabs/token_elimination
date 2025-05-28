8de271706989626a17d05a07f2b5c46d517351da
Michael Beale <michael.beale@intel.com>
2025-05-28 10:47:21 -0700

    updating requirements to fix open CVE's. Manual update because Dependabot could not resolve.

--- src/index.py
+++ src/index.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
-import os
 import pickle
 from typing import List, Tuple
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 import json
-import parser
 import sys
 from pathlib import Path
 
